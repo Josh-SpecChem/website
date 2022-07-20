f2449c1a82df3f71cd5588154d10174c828dc361
Josh-SpecChem <106373289+Josh-SpecChem@users.noreply.github.com>
2022-07-20 12:14:29 -0500

    Add files via upload

---
+++ src/form-process.php
@@ -0,0 +1,75 @@
+<?php
+
+$errorMSG = "";
+
+// FIRSTNAME
+if (empty($_POST["firstname"])) {
+    $errorMSG = "First name is required ";
+} else {
+    $name = $_POST["firstname"];
+}
+
+// LASTNAME
+if (empty($_POST["lastname"])) {
+    $errorMSG = "Last name is required ";
+} else {
+    $name = $_POST["lastname"];
+}
+
+// EMAIL
+if (empty($_POST["email"])) {
+    $errorMSG .= "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+// PHONE
+if (empty($_POST["phone"])) {
+    $errorMSG .= "Phone is required ";
+} else {
+    $email = $_POST["phone"];
+}
+
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "Message is required ";
+} else {
+    $message = $_POST["message"];
+}
+
+$EmailTo = "jshepherd@specchem.com";
+$Subject = "You Fucking Did It!";
+
+// prepare email body text
+$Body = "";
+$Body .= "First Name: ";
+$Body .= $firstname;
+$Body .= "\n";
+$Body .= "Last Name: ";
+$Body .= $lastname;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Phone: ";
+$Body .= $phone;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+
+?>
